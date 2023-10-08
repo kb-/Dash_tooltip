abcb205bbeb88c4b4ce928c32fd94057cd08451a
kb- <olivier.around.the.world@gmail.com>
2023-10-08 13:48:01 +0200

    added tested python versions

--- setup.py
+++ setup.py
@@ -15,6 +15,11 @@ setup(
     packages=find_packages(),  # Automatically discover and include all packages
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
